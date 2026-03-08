05481e412d19d29d8d67d1f96396692b9d28ab79
jae_m3 <yoonjaelee@MacBook-Pro-10.local>
2026-03-08 18:49:45 +0900

    fix board3

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,22 +1,22 @@
-// import { createBrowserClient } from '@supabase/ssr'
-
-// export function createClient() {
-
-//   const url = process.env.NEXT_PUBLIC_SUPABASE_URL
-//   const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-
-//   if (!url || !key) {
-//     throw new Error("Supabase env missing")
-//   }
-
-//   return createBrowserClient(url, key)
-// }
-
-import { createBrowserClient } from "@supabase/ssr"
+import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
-  return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  )
+
+  const url = process.env.NEXT_PUBLIC_SUPABASE_URL
+  const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
+  if (!url || !key) {
+    throw new Error("Supabase env missing")
+  }
+
+  return createBrowserClient(url, key)
 }
+
+// import { createBrowserClient } from "@supabase/ssr"
+
+// export function createClient() {
+//   return createBrowserClient(
+//     process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+//   )
+// }
